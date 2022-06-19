875d29ba87e8b773e5f4a51d6073a3ac14ed41fc
mahdita97 <taala.mahdy@gmail.com>
2022-06-19 15:39:59 +0430

    feat: Update article lifecycle
    - add arabic slugify functionality

--- src/api/article/content-types/article/lifecycles.js
+++ src/api/article/content-types/article/lifecycles.js
@@ -1,20 +1,70 @@
 const slugify = require("slug");
 
+slugify.extend({
+  ء: "ء",
+  آ: "آ",
+  أ: "أ",
+  ؤ: "ؤ",
+  إ: "إ",
+  ئ: "ئ",
+  ا: "ا",
+  ب: "ب",
+  ة: "ة",
+  ت: "ت",
+  ث: "ث",
+  ج: "ج",
+  ح: "ح",
+  خ: "خ",
+  د: "د",
+  ذ: "ذ",
+  ر: "ر",
+  ز: "ز",
+  س: "س",
+  ش: "ش",
+  ص: "ص",
+  ض: "ض",
+  ط: "ط",
+  ظ: "ظ",
+  ع: "ع",
+  غ: "غ",
+  ف: "ف",
+  ق: "ق",
+  ك: "ك",
+  ل: "ل",
+  م: "م",
+  ن: "ن",
+  ه: "ه",
+  و: "و",
+  ى: "ى",
+  ي: "ي",
+  "ً": "",
+  "ٌ": "",
+  "ٍ": "",
+  "َ": "",
+  "ُ": "",
+  "ِ": "",
+  "ْ": "",
+  پ: "پ",
+  چ: "چ",
+  ژ: "ژ",
+  ک: "ک",
+  گ: "گ",
+  ی: "ی",
+});
+
 module.exports = {
   async beforeCreate(event) {
     if (event.params.data.title) {
-      event.params.data.slug = slugify(event.params.data.title, {
-        lower: true,
-      });
-      console.log("===========>beforeCreate", event.params.data);
+      event.params.data.slug =
+        event.params.where.id + "-" + slugify(event.params.data.title);
+      // console.log("===========>beforeCreate", event.params);
     }
   },
   async beforeUpdate(event) {
     if (event.params.data.title) {
-      event.params.data.slug = slugify(event.params.data.title, {
-        lower: true,
-      });
-      console.log("===========>beforeUpdate", event.params.data);
+      event.params.data.slug =
+        event.params.where.id + "-" + slugify(event.params.data.title);
+      // console.log("===========>beforeUpdate", event.params);
     }
   },
 };
